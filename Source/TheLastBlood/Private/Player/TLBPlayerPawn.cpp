e7d3a7bf0895ce57d9db4c4eaf712eb9872cb8c2
jffry <36561967+jffrey@users.noreply.github.com>
2021-05-03 23:12:48 +0300

    Default player pawn with 1P mesh and camera creation

---
+++ Source/TheLastBlood/Private/Player/TLBPlayerPawn.cpp
@@ -0,0 +1,42 @@
+
+#include "Player/TLBPlayerPawn.h"
+
+// Constructor
+ATLBPlayerPawn::ATLBPlayerPawn()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	
+	/** Create components */
+	Mesh_1P = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FPMesh"));
+	Camera_1P = CreateDefaultSubobject<UCameraComponent>(TEXT("FPCamera"));
+
+	/** Attach camera to root (capsule) and allow to look around */
+	Camera_1P->SetupAttachment(RootComponent);
+	Camera_1P->bUsePawnControlRotation = true;
+	
+	/** Attach first person mesh to camera */
+	Mesh_1P->SetupAttachment(Camera_1P);
+
+	/** Make sure that local player can only see FP mesh, and remotes can only see third person mesh  */
+	GetMesh()->bOwnerNoSee = true;
+	GetMesh()->bCastHiddenShadow = true;
+	Mesh_1P->bOnlyOwnerSee = true;
+	Mesh_1P->CastShadow = false;
+}
+
+void ATLBPlayerPawn::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ATLBPlayerPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ATLBPlayerPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/TheLastBlood/Public/Player/TLBPlayerPawn.h
@@ -0,0 +1,32 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/Character.h"
+#include "TLBPlayerPawn.generated.h"
+
+UCLASS()
+class THELASTBLOOD_API ATLBPlayerPawn : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ATLBPlayerPawn();
+
+	/** First-person mesh */
+	UPROPERTY(EditDefaultsOnly)
+	USkeletalMeshComponent* Mesh_1P;
+
+	/** First-person and main camera */
+	UPROPERTY(EditDefaultsOnly)
+	UCameraComponent* Camera_1P;
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
