84f552b4d32618c28fbea490e105419c85e64092
jffry <36561967+jffrey@users.noreply.github.com>
2021-05-03 23:37:52 +0300

    Add custom movement component

---
+++ Source/TheLastBlood/Private/Player/Components/TLBCharacterMovement.cpp
@@ -0,0 +1,3 @@
+
+#include "Player/Components/TLBCharacterMovement.h"
+
---
+++ Source/TheLastBlood/Public/Player/Components/TLBCharacterMovement.h
@@ -0,0 +1,13 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "TLBCharacterMovement.generated.h"
+
+UCLASS()
+class THELASTBLOOD_API UTLBCharacterMovement : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+	
+};
